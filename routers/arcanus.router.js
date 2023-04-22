246613b9b6d65a1a84be00c8581ff89a787e53f6
Thamiris Cristina <thamiris_cristina_962@hotmail.com>
2023-04-22 16:19:07 -0300

    skills

--- routers/arcanus.router.js
+++ routers/arcanus.router.js
@@ -4,5 +4,7 @@ const arcanusController = require('../controllers/arcanus.controller');
 
 router.post('/getArcanus', arcanusController.getArcanus);
 router.post('/setArcanus', arcanusController.setArcanus);
+router.post('/getSkills', arcanusController.getSkills);
+router.post('/setSkills', arcanusController.setSkills);
 
 module.exports = router;
