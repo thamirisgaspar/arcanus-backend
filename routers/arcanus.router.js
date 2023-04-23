c39002388a57ad756850ec6e709f8f2ff60e4226
Thamiris Cristina <thamiris_cristina_962@hotmail.com>
2023-04-22 21:33:56 -0300

    arcanus-backend

--- routers/arcanus.router.js
+++ routers/arcanus.router.js
@@ -2,12 +2,4 @@ const express = require('express');
 const router = express.Router();
 const arcanusController = require('../controllers/arcanus.controller');
 
-router.post('/getArcanus', arcanusController.getArcanus);
-router.post('/setArcanus', arcanusController.setArcanus);
-router.post('/getSkills', arcanusController.getSkills);
-router.post('/setSkills', arcanusController.setSkills);
-router.post('/getOthers', arcanusController.getOthers);
-router.post('/setOthers', arcanusController.setOthers);
-router.post('/changed', arcanusController.changed);
-
 module.exports = router;
